0ac5f7614f4c632e845d1a4288a30e2ab9425af5
szemenyeim <szemenyei@iit.bme.hu>
2020-09-04 11:49:24 +0200

    útmutató update

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -13,25 +13,25 @@ class KalmanFilter(object):
     def __init__(self) -> None:
         super().__init__()
 
-        # define state transition matrix for 3D positions and velocities
+        #TODO: define state transition matrix for 3D positions and velocities
         A = []
 
-        # define observation matrix for 3D position measurements
+        #TODO: define observation matrix for 3D position measurements
         C = []
 
 
-        # set covariance matrices (use small values)
+        #TODO: set covariance matrices (use small values)
         self.P = None
         Q = None  # process noise covariance
         R = None  # measurement noise covariance
 
         # initialize the state vector
-        # None-s should be replaced
+        #TODO: None-s should be replaced
         x0, y0, z0 = None, None, None
         vx0, vy0, vz0 = None, None, None
         self.x = [x0, y0, z0, vx0, vy0, vz0]
 
-        # create the KF
+        #TODO: create the KF
         self.kf = pykalman.KalmanFilter(transition_matrices=None,
                                         observation_matrices=None,
                                         initial_state_mean=None,
