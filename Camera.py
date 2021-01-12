33cc5f19ec9c48d186e77e258c332e7630d4b7f4
Márton Szemenyei <szemenyei@iit.bme.hu>
2021-01-12 08:27:16 +0100

    realsense added

--- Camera.py
+++ Camera.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2
-#from primesense import openni2
-#from primesense import _openni2 as c_api
+from primesense import openni2
+from primesense import _openni2 as c_api
+import pyrealsense2 as rs
 import os.path as osp
 from glob import glob1
 import re
-#import platform
 
-#rel = platform.linux_distribution() #returns release version
+rel = 2 #returns release version
 
 def tryint(s):
     try:
@@ -21,9 +21,9 @@ def alphanum_key(s):
     """
     return [ tryint(c) for c in re.split('([0-9]+)', s) ]
 
-'''class Camera(object):
+class Camera(object):
 
-    if rel[1] == '18.04':
+    if rel == 0:
         def getImages(self):
             import freenect
             img, _ = freenect.sync_get_video()
@@ -31,7 +31,7 @@ def alphanum_key(s):
             img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
             return img, depth
 
-    else:
+    elif rel == 1:
         def __init__(self):
             openni2.initialize("/home/labor/Downloads/OpenNI_2.3.0.55/Linux/OpenNI-Linux-x64-2.3.0.55/Redist")
             self.dev = openni2.Device.open_any()
@@ -60,7 +60,33 @@ def alphanum_key(s):
             img.shape = (480, 640, 3)
             img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
-            return img, depth'''
+            return img, depth
+    else:
+        def __init__(self):
+            self.pipeline = rs.pipeline()
+            config = rs.config()
+            config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+            config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+            align_to = rs.stream.color
+            self.align = rs.align(align_to)
+
+            # Start streaming
+            self.pipeline.start(config)
+
+        def getImages(self):
+            frames = self.pipeline.wait_for_frames()
+            frames = self.align.process(frames)
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                return
+
+            # Convert images to numpy arrays
+            depth = np.asanyarray(depth_frame.get_data())
+            img = np.asanyarray(color_frame.get_data())
+
+            return img, depth
 
 class FolderCam(object):
     def __init__(self, path):
